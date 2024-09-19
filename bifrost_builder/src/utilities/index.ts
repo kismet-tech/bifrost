1cc347580d06aec800e9dbcdddae7350c24c8123
Sudhanshu Pandey <sp6370@nyu.edu>
2024-09-18 21:22:05 -0400

    chore: refactoring update

--- bifrost_builder/src/utilities/index.ts
+++ bifrost_builder/src/utilities/index.ts
@@ -1,38 +1,38 @@
 import { recordWebsiteVisitFromAdLinkUrl } from "@/config";
 import axios from "axios";
 
-export const getVisitorId = (): string | undefined => {
+export const getBifrostTravelerId = (): string | undefined => {
     return document.cookie
         .split("; ")
-        .find((row) => row.startsWith("visitorId="))
+        .find((row) => row.startsWith("bifrostTravelerId="))
         ?.split("=")[1];
 };
 
 export const handleBifrostTraveler = async (url: URL) => {
-    const maybeBifrostTravelerId: string | undefined = url.searchParams.get("bifrostTravelerId") || undefined;
+    const maybeTemporaryBifrostTravelerId: string | undefined = url.searchParams.get("temporaryBifrostTravelerId") || undefined;
 
-    if (maybeBifrostTravelerId) {
-        const visitorId = getVisitorId();
-        console.log("visitorId", visitorId);
+    if (maybeTemporaryBifrostTravelerId) {
+        const bifrostTravelerId = getBifrostTravelerId();
+        console.log("bifrostTravelerId", bifrostTravelerId);
 
         try {
             const response = await axios.post(recordWebsiteVisitFromAdLinkUrl, {
-                bifrostTravelerId: maybeBifrostTravelerId,
-                visitorId: visitorId
+                temporaryBifrostTravelerId: maybeTemporaryBifrostTravelerId,
+                bifrostTravelerId: bifrostTravelerId
             });
 
             console.log("response", response);
 
-            const receivedVisitorId = response.data.success.visitorId;
-            console.log("Received visitorId:", receivedVisitorId);
+            const receivedBifrostTravelerId = response.data.success.bifrostTravelerId;
+            console.log("Received visitorId:", receivedBifrostTravelerId);
 
-            if (receivedVisitorId) {
+            if (receivedBifrostTravelerId) {
 
                 const domain = window.location.hostname;
                 const secure = window.location.protocol === "https:" ? "; secure" : "";
                 const expirationDate = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toUTCString();
 
-                document.cookie = `visitorId=${receivedVisitorId}; path=/; domain=${domain}${secure}; SameSite=Strict; expires=${expirationDate}`;
+                document.cookie = `bifrostTravelerId=${receivedBifrostTravelerId}; path=/; domain=${domain}${secure}; SameSite=Strict; expires=${expirationDate}`;
             }
         } catch (error) {
             console.error("Error recording website visit:", error);
