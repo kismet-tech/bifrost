845396f62c123ff7a5a6072debefe725ec907ba8
Sudhanshu Pandey <sp6370@nyu.edu>
2024-09-19 15:37:58 -0400

    chore: wip

--- builderv2/bifrost_builder/src/main.tsx
+++ builderv2/bifrost_builder/src/main.tsx
@@ -1,11 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import './instrument'
+declare global {
+  interface Window {
+    hasBifrostLoaded?: boolean;
+  }
+}
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export const main = () => {
+  if (!window.hasBifrostLoaded) {
+    window.hasBifrostLoaded = true;
+
+    console.log("🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊");
+    console.log("🧊  Placing the Bifrost 🧊");
+    console.log("🧊  App Version: " + __APP_VERSION__ + " 🧊");
+    console.log("🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊");
+
+    const bifrostConfiguration: BifrostConfiguration =
+      getBifrostConfiguration();
+
+    const url = new URL(window.location.href);
+    handleBifrostTraveler(url);
+
+    if (
+      document.readyState === "complete" ||
+      document.readyState === "interactive"
+    ) {
+      // If the DOM is already loaded, replace the form immediately
+      injectDynamicRFP({ bifrostConfiguration });
+    } else {
+      // Otherwise, set an event listener
+      document.addEventListener("DOMContentLoaded", () => {
+        injectDynamicRFP({ bifrostConfiguration });
+      });
+    }
+  }
+};
+
+main();
--- builderv2/bifrost_builder/vite.config.ts
+++ builderv2/bifrost_builder/vite.config.ts
@@ -1,7 +1,26 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const entryFile = "src/main.tsx";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "tourii",
+    project: "bifrost_v2"
+  })],
+
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      input: { main: entryFile },
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  }
 })
