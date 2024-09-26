49860980727af52fe4728f30c3d1d0bf1857059b
Blake Zimmerman <blake.zimmerman@icloud.com>
2024-09-26 12:20:51 -0400

    Add Select and hook up text inputs (#33)

--- bifrost_builder/src/components/KismetForm/components/FormTextField.tsx
+++ bifrost_builder/src/components/KismetForm/components/FormTextField.tsx
@@ -1,20 +1,12 @@
-import styled from "styled-components";
-import { TextInputFormBlockConfiguration } from "../models";
-import { FormLabel } from "./FormLabel";
-import { FormField } from "./FormField";
-import { ChangeEventHandler, useEffect, useState } from "react";
 import {
-  attemptToPrefillKismetFieldUsingPriorResponses,
   PrefilledBifrostFormValueType,
+  attemptToPrefillKismetFieldUsingPriorResponses,
 } from "@/api/attemptToPrefillKismetFieldUsingPriorResponses";
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.5rem; /* Equivalent to p-2 */
-  border: 1px solid #d1d5db; /* Equivalent to border-gray-300 */
-  border-radius: 0.25rem;
-  box-sizing: border-box;
-`;
+import { ChangeEventHandler, useEffect, useState } from "react";
+import { TextInputFormBlockConfiguration } from "../models";
+import { FormField } from "./FormField";
+import { FormLabel } from "./FormLabel";
+import { Input } from "@/components/ui/input";
 
 export interface FormTextFieldProps {
   configuration: TextInputFormBlockConfiguration;
